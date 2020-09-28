2d193fc525b42223f182fefda238a1b93ab8deb4
tadasgrinevic <41401466+tadasgrinevic@users.noreply.github.com>
2020-09-28 01:23:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
 try{
-$soapclient = new SoapClient('http://www.holidaywebservice.com/HolidayService_v2/HolidayService2.asmx?wsdl');
-$response = $soapclient->GetCountriesAvailable();
-var_dump($response);
-echo '<br><br><br>';
-$array = json_decode(json_encode($response), true);
-print_r($array);
-echo '<br><br><br>';
-echo  $array['GetCountriesAvailableResult']['CountryCode']['5']['Description'];
-	  echo '<br><br><br>';
+	$soapclient = new SoapClient('http://www.holidaywebservice.com/HolidayService_v2/HolidayService2.asmx?wsdl');
+	$response = $soapclient->GetCountriesAvailable();
+	var_dump($response);
+	echo '<br><br><br>';
+	$array = json_decode(json_encode($response), true);
+	print_r($array);
+	echo '<br><br><br>';
+	echo  $array['GetCountriesAvailableResult']['CountryCode']['5']['Description'];
+	echo '<br><br><br>';
 	foreach($array as $item) {
 		echo '<pre>'; var_dump($item);
 	}  
